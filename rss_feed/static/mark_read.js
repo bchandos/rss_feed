5ed17495d4de49ecceb5ccb0b51dd2d2a69ac7dc
Bill Chandos <bill.chandos@gmail.com>
2018-10-14 11:05:36 -0700

    Create mark read div

--- rss_feed/static/mark_read.js
+++ rss_feed/static/mark_read.js
@@ -1,7 +1,7 @@
 $(function () {
-    $('article.item').on('click', function () {
+    $('.marker').on('click', function () {
         $.getJSON($SCRIPT_ROOT + '/_mark_read', {
-            id: $(this).attr('id')
+            id: $(this).attr('id').slice(3)
         }, function (data) {
             $('article#' + data.id).fadeOut('200', function () {
                 $('article#' + data.id).removeClass("unread").addClass("read");
