3f21f3f4c7b2469ba389e49a665857e64a1b2543
Bill Chandos <bill.chandos@gmail.com>
2018-10-22 20:13:26 -0700

    Implemented user_items table to track read status by user

--- rss_feed/rss_feed.py
+++ rss_feed/rss_feed.py
@@ -22,12 +22,13 @@ def index():
     user_id = g.user['id']
     items = db.execute('SELECT items.id, feeds.feed_name, items.feed_id, items.title, '
                        'items.link, items.description, items.publication_date, '
-                       'items.guid, user_feeds.user_id, items.read '
+                       'items.guid, user_feeds.user_id, user_items.read '
                        'FROM items '
                        'INNER JOIN feeds ON items.feed_id = feeds.id '
                        'INNER JOIN user_feeds on items.feed_id = user_feeds.feed_id '
-                       'WHERE user_feeds.user_id = (?) '
-                       'ORDER BY items.publication_date DESC', (user_id,)).fetchall()
+                       'INNER JOIN user_items on items.id = user_items.item_id '
+                       'WHERE user_feeds.user_id = ? AND user_items.user_id = ? '
+                       'ORDER BY items.publication_date DESC', (user_id, user_id)).fetchall()
     return render_template('rss_feed/index.html', items=items)
 
 
@@ -40,13 +41,13 @@ def feed_index(feed_id):
         'SELECT feed_name FROM feeds WHERE id = ?', (feed_id,)).fetchone()['feed_name']
     items = db.execute('SELECT items.id, feeds.feed_name, items.feed_id, items.title, '
                        'items.link, items.description, items.publication_date, '
-                       'items.guid, user_feeds.user_id, items.read '
+                       'items.guid, user_feeds.user_id, user_items.read '
                        'FROM items '
                        'INNER JOIN feeds ON items.feed_id = feeds.id '
                        'INNER JOIN user_feeds on items.feed_id = user_feeds.feed_id '
-                       'WHERE user_feeds.user_id = ? '
-                       'AND items.feed_id = ? '
-                       'ORDER BY items.publication_date DESC', (user_id, feed_id)).fetchall()
+                       'INNER JOIN user_items on items.id = user_items.item_id '
+                       'WHERE user_feeds.user_id = ? AND user_items.user_id = ? '
+                       'ORDER BY items.publication_date DESC', (user_id, user_id)).fetchall()
     return render_template('rss_feed/index.html', items=items, feed_name=feed_name, feed_id=feed_id)
 
 
@@ -130,14 +131,15 @@ def delete_feed(id):
 @bp.route('/update/<int:feed_id>')
 @login_required
 def get_items(feed_id):
+    user_id = g.user['id']
     if 'feed_group' in g.user.keys():
         if feed_id and str(feed_id) in g.user['feed_group']:
             feed = get_feed(feed_id)
-            download_items(feed['feed_url'], feed_id)
+            download_items(feed['feed_url'], feed_id, user_id)
         elif not feed_id:
             for user_feed_id in g.user['feed_group'].split(','):
                 feed = get_feed(int(user_feed_id))
-                download_items(feed['feed_url'], user_feed_id)
+                download_items(feed['feed_url'], user_feed_id, user_id)
         else:
             abort(404, 'No such feed.')
     else:
@@ -147,7 +149,7 @@ def get_items(feed_id):
     return redirect(url_for('index'))
 
 
-def download_items(url, feed_id):
+def download_items(url, feed_id, user_id):
     db = get_db()
     with urlopen(url) as f:
         if f.getcode() == 200 and 'xml' in f.getheader('Content-Type'):
@@ -160,8 +162,10 @@ def download_items(url, feed_id):
                 publication_date = datetime.timestamp(
                     parse(item.find('pubDate').text))
                 guid = item.find('guid').text
-                db.execute('INSERT OR IGNORE INTO items (feed_id, title, link, description, publication_date, guid) VALUES (?, ?, ?, ?, ?, ?)',
-                           (feed_id, title, link, description, publication_date, guid))
+                item_id = db.execute('INSERT OR IGNORE INTO items (feed_id, title, link, description, publication_date, guid) VALUES (?, ?, ?, ?, ?, ?)',
+                                     (feed_id, title, link, description, publication_date, guid)).lastrowid
+                db.execute(
+                    'INSERT OR IGNORE INTO user_items (user_id, item_id, read) VALUES (?, ?, 0)', (user_id, item_id))
             db.commit()
 
 
@@ -173,9 +177,11 @@ def datetimeformat(value, format='%m-%d-%Y @ %H:%M'):
 
 @bp.route('/_mark_read')
 def mark_read():
+    user_id = g.user['id']
     id = request.args.get('id', 0, type=int)
     db = get_db()
     if id:
-        db.execute('UPDATE items SET read = 1 WHERE id = ?', (id,))
+        db.execute(
+            'UPDATE user_items SET read = 1 WHERE item_id = ? AND user_id = ?', (id, user_id))
         db.commit()
         return jsonify(id=id, read='Read')
