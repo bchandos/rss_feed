b88d3b1aea9c6ef7bbb97518367b0971500eec48
Bill Chandos <bill.chandos@gmail.com>
2019-04-06 15:22:36 -0700

    remove display:none inline style

--- rss_feed/static/mark_read.js
+++ rss_feed/static/mark_read.js
@@ -3,8 +3,11 @@ $(function () {
         $.getJSON($SCRIPT_ROOT + '/_mark_read', {
             id: $(this).attr('id').slice(3)
         }, function (data) {
-            $('article#' + data.id).fadeOut('200', function () {
+            $('article#' + data.id).fadeOut("200", function () {
                 $('article#' + data.id).removeClass("unread").addClass("read invisible");
+                /*  removes the display: none added by fadeOut
+                    and allow the standard CSS to control display */
+                $('article#' + data.id).css("display", "");
             });
         });
         return false;
