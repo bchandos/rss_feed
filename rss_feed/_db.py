c0ffb74bb3838ed3cf115f06cc82bf8e4b35d565
Bill <bill.chandos@gmail.com>
2020-11-07 16:05:11 -0800

    psycopg2 perhaps

--- rss_feed/_db.py
+++ rss_feed/_db.py
@@ -1,4 +1,5 @@
-import sqlite3
+# import sqlite3
+import psycopg2
 
 import click
 from flask import current_app, g
@@ -6,8 +7,9 @@ from flask.cli import with_appcontext
 
 def get_db():
     if 'db' not in g:
-        g.db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
-        g.db.row_factory = sqlite3.Row
+        # g.db = sqlite3.connect(current_app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+        # g.db.row_factory = sqlite3.Row
+        g.db = psycopg2.connect(current_app.config['DATABASE'])
     return g.db
 
 def close_db(e=None):
