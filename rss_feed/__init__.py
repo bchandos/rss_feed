c7a42ab808bf364703ca0eb1510c3b6261d0b132
bchandos <contact@billchandos.dev>
2023-04-09 18:35:17 -0700

    Try to fix static pathing, again

--- rss_feed/__init__.py
+++ rss_feed/__init__.py
@@ -15,7 +15,7 @@ def create_app(test_config=None):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # app.config["SQLALCHEMY_ECHO"] = True
-    app.
+    
     if test_config is None:
         # Load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
